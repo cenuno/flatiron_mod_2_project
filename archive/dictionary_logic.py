e0ec0a0b5158d35153eb3d25a4a2cc41dd69eb1e
cenuno <24255819+cenuno@users.noreply.github.com>
2019-05-09 10:35:15 -0700

    Add additional tests

---
+++ archive/dictionary_logic.py
@@ -0,0 +1,7 @@
+bball = {"chicago": 1, "cleveland": [1, 2, 3], "los angeles": [6, 7]}
+
+output = {}
+for key, value in bball.items():
+    output[key] = value
+
+print(output)
--- archive/understand_random_seed.py
+++ archive/understand_random_seed.py
@@ -12,3 +12,22 @@ print(
     I know a random stranger who ate {random_int} slices of pizza in an hour!
     """
 )
+
+np.random.seed(2019)
+output_1 = []
+for num in range(10):
+    output_1.append(int(np.random.choice(pizza_slices, size=1)))
+print(output_1)
+
+np.random.seed(2019)
+output_2 = []
+for num in range(10):
+    output_2.append(int(np.random.choice(pizza_slices, size=1)))
+print(output_2)
+
+print(f"Are output 1 and output 2 the same? {output_1 == output_2}")
+
+from random import randint
+np.random.seed(2019)
+print(randint(0, 10))
+print(randint(0, 10))
